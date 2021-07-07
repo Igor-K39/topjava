da6d3564c1c19875119a4fd3f253054057ebd38b
Igor Kopyshev <kopyshev.sd@gmail.com>
2021-07-07 11:17:23 +0300

    HW_2_Optional: Implemented an imitation of user authentication

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -18,4 +18,13 @@ public class UserServlet extends HttpServlet {
         log.debug("forward to users");
         request.getRequestDispatcher("/users.jsp").forward(request, response);
     }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String user = request.getParameter("user");
+        int userId = user != null ? Integer.parseInt(user) : 1;
+        SecurityUtil.setAuthUserId(userId);
+        log.debug("redirect to meals");
+        response.sendRedirect("meals");
+    }
 }
